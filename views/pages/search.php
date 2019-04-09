ff97a20610f914a6e519790b97a95dfb5ee4cf03
Lissandre <lissandre.pasdeloup@hetic.net>
2019-04-09 17:18:31 +0200

    :fire: Remove some text

--- views/pages/search.php
+++ views/pages/search.php
@@ -1,15 +1,15 @@
 <?php include '../views/partials/header.php' ?>
 
-<h1>Home</h1>
 <div class="dispResearch">
 <?php if(!empty($plantsList)): ?>
     <?php foreach($plantsList as $key): ?>
         <div class="plantsText">
             <img src="<?= !empty($key[3][0]) ? $key[3][0]->url : "placeholer" ?>" alt="plant picture" class="plant_image_home">
-            <p class="plant_name">Name : <?= $key[0] ?></p>
+            <p class="plant_name"><?= $key[0] ?></p>
             <a href="<?= 'plant/'.$key[2] ?>" class="plant_link">
-                <img src="<?= URL ?>assets/images/plus.svg" alt="petit plus"> Add
-                </a>
+                <img src="<?= URL ?>assets/images/plus.svg" alt="petit plus"> 
+                Add
+            </a>
         </div>
     <?php endforeach; ?>
 <?php elseif(empty($plantsList) && !empty($_GET['name'])): ?>
