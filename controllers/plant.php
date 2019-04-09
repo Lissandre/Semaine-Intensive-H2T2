457c95377d028549362902f3e7c52cc082d0417b
Lissandre <lissandre.pasdeloup@hetic.net>
2019-04-09 15:28:56 +0200

    :sparkles: Get all informations from a plant

--- controllers/plant.php
+++ controllers/plant.php
@@ -7,6 +7,21 @@ $title = 'plant datas';
  * >> REST API
  */
  //Get q param
- $q = !empty($_GET['q']) ? $_GET['q'] : 'home';
+$id = basename($_GET['q']);
+// Create URL
+$url = 'https://trefle.io/api/plants/'.$id.'?token='.TOKEN_TREFLE;
+// Make request to API if the id exist
+if(get_headers($url)[0]=='HTTP/1.1 200 OK')
+{
+    $result = file_get_contents($url);
+    // Decode JSON
+    $result = json_decode($result);
+}else
+{
+    $result = 'NOT FOUND'; 
+}
+echo '<pre>';
+print_r($result);
+echo '</pre>';
 
 include '../views/pages/plant.php';
