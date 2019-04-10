f7e7278dbf6a60fe7ff08649dc6789e99013e1f9
Lissandre <lissandre.pasdeloup@hetic.net>
2019-04-10 12:32:38 +0200

    :sparkles: Cache for plants information

--- controllers/plant.php
+++ controllers/plant.php
@@ -11,31 +11,42 @@ $id = basename($_GET['q']);
 // Create URL
 $url = 'https://trefle.io/api/plants/'.$id.'?token='.TOKEN_TREFLE;
 // Make request to API if the id exist
-if(get_headers($url)[0]=='HTTP/1.1 200 OK')
+// Cache info
+$cacheKey = md5($url);
+$cachePath = '../cache/'.$cacheKey;
+//If Request has been done before and younger than a week
+if(file_exists($cachePath) && time() - filemtime($cachePath) < 604800)
 {
-    $result = file_get_contents($url);
-    // Decode JSON
-    $result = json_decode($result);
-    //Saving important informations in an array
-    $result = [
-        'name' => $result->common_name,
-        'scientific_name' => $result->scientific_name,
-        'shape_and_orientation' => $result->main_species->specifications->shape_and_orientation,
-        'mature_height' => $result->main_species->specifications->mature_height->cm,
-        'lifespan' => $result->main_species->specifications->lifespan,
-        'growth_period' => $result->main_species->specifications->growth_period,
-        'growth_habit' => $result->main_species->specifications->growth_habit,
-        'images' => $result->images,
-        'temperature_minimum' => $result->main_species->growth->temperature_minimum->deg_c,
-        'precipitation_minimum' => $result->main_species->growth->precipitation_minimum->cm,
-        'precipitation_maximum' => $result->main_species->growth->precipitation_maximum->cm,
-        'flower_color' => $result->main_species->flower->color,
-        'duration' => $result->duration
-    ];
-}else
-{
-    //If the plant is not found
-    $result = 'NOT FOUND'; 
+    $result = file_get_contents($cachePath);
+    $result = json_decode($result, true);
+}else{
+    if(get_headers($url)[0]=='HTTP/1.1 200 OK')
+    {
+        $result = file_get_contents($url);
+        // Decode JSON
+        $result = json_decode($result);
+        //Saving important informations in an array
+        $result = [
+            'name' => $result->common_name,
+            'scientific_name' => $result->scientific_name,
+            'shape_and_orientation' => $result->main_species->specifications->shape_and_orientation,
+            'mature_height' => $result->main_species->specifications->mature_height->cm,
+            'lifespan' => $result->main_species->specifications->lifespan,
+            'growth_period' => $result->main_species->specifications->growth_period,
+            'growth_habit' => $result->main_species->specifications->growth_habit,
+            'images' => $result->images,
+            'temperature_minimum' => $result->main_species->growth->temperature_minimum->deg_c,
+            'precipitation_minimum' => $result->main_species->growth->precipitation_minimum->cm,
+            'precipitation_maximum' => $result->main_species->growth->precipitation_maximum->cm,
+            'flower_color' => $result->main_species->flower->color,
+            'duration' => $result->duration
+        ];
+        $searchSave = json_encode($result);
+        file_put_contents($cachePath, $searchSave);
+    }else
+    {
+        //If the plant is not found
+        $result = 'NOT FOUND'; 
+    }
 }
-
 include '../views/pages/plant.php';
