25fcf24f6b97e232241f6e1c2a1fb75976ada3a0
ClemPrt <clement.parot@hetic.net>
2019-04-10 21:17:16 +0200

    Add homepage + stylehome page

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,5 +1,51 @@
 <?php include '../views/partials/header.php' ?>
 
+<div class="content_home">
+    <div class="container">
+        <div class="background_main">
+        <h1>
+            Manage your plant
+        </h1>
+        <p class="desc_main">
+            You always wanted to have a flowered, green and breathing apartment? A nice flat to live without necessarily
+            investing yourself? Make your house greener without necessarily having the time or knowledge ... without
+            necessarily having a green thumb ?
+        </p>
+        <p class="button"> 
+            Get started
+        </p>
+    </div>
+    <div class="content_info m-30">
+        <p class="title">
+        Here, we will help you achieve this goal.
+        So, how does it work ?
+        </p>
+        <p class="desc">
+        After registering or logging in, you’ll either give us the name of your plant, either take a picture of it for
+        us to recognize it.
+        </p>
+        <img src="../images/plantbg.png" alt="">
+    </div>
+    <div class="content_info">
+        <p class="desc">
+        We will then give you lots of information about it. Such as her family, her origins, how she evolves, her
+        characteristics, her needs and many more. In order not to get lost, you will create a record for your plant with
+        the information you find necessary.
+        </p>
+        <img src="../images/plantbg.png" alt="">
+    </div>
+    <div class="content_info">
+        <p class="desc">
+        From here we will help you ensure a peaceful life for your plant. You’ll be able to link the calendar of needs
+        of your plant to your agenda. You will receive reminders with instructions !
+        </p>
+        <img src="../images/plantbg.png" alt="">
+    </div>
+    <div class="content_info">
+      <p class="title">
+        Thereupon, you just have to start the adventure with your plant!
+      </p>
+    </div>
+  </div>
 
-
-<?php include '../views/partials/footer.php' ?>
+<?php include '../views/partials/footer.php' ?> 
