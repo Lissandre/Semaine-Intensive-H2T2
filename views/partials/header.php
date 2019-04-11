781d171ee3b09998f2dd26e8c8a9d989b5f70a52
MattyxBaguette <mathis.gadie@hetic.net>
2019-04-11 12:24:25 +0200

    :lipstick: fav icon added + login style fixed

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= !empty($title) ? $title : '' ?></title>
+    <link rel="icon" href="<?= URL ?>assets/images/logo.png">
     <link rel="stylesheet" href="<?= URL ?>assets/style.css">
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,500" rel="stylesheet">
 </head>
