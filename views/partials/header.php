04f302d9159b646ebc991cac68c366453d8b5c81
Lissandre <lissandre.pasdeloup@hetic.net>
2019-04-11 16:39:40 +0200

    :bug: Correction redirection search

--- views/partials/header.php
+++ views/partials/header.php
@@ -18,7 +18,7 @@
         </a>
 
         <?php if($_GET['q']!='home' && $_GET['q']!=''): ?>
-        <form class="header_search" action="search" method="get">
+        <form class="header_search" action="<?= URL ?>search" method="get">
             <label for="name">
             </label>
             <input class="search_bar_header" type="text" name="name" id="name" autocomplete="off" placeholder="Search your plant" >
